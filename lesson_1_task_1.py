9ac604d0550beda4c7a5069822ee4b2cd8d6e575
AngelinaGamova <gamang63@gmail.com>
2023-09-26 14:40:18 +0300

    выполнила задания по первому уроку

---
+++ lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = "Rezvondrena"
+print (my_name)
---
+++ lesson_1_task_7.py
@@ -0,0 +1,56 @@
+#def odin (number):
+   #print(number)
+
+# odin(8)
+# odin(8)
+# odin(0)
+# odin(0)
+# odin(5)
+# odin(5)
+# odin(5)
+# odin(3)
+# odin(5)
+# odin(3)
+# odin(5)
+
+def raz ():
+    print (1)
+    
+def dva ():
+    print (2)
+
+def tri ():
+    print (3, end="")
+    
+def chetire ():
+    print (4)
+    
+def pyat ():
+    print (5, end="")
+       
+def shest ():
+    print (6)
+
+def sem ():
+    print (7)
+
+def vosem ():
+    print (8, end="")
+    
+def devyat ():
+    print (9)
+    
+def nol ():
+    print (0, end="")
+   
+vosem()
+vosem() 
+nol()
+nol()
+pyat()
+pyat()
+pyat()
+tri()
+pyat()
+tri()
+pyat()
